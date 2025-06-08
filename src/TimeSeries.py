d12839ae1040bc424ff2d94f617b76fe2e1bf129
matuneville <nevillematias@gmail.com>
2025-06-08 20:48:28 -0300

    add Frecuency plot, refactor de Tendency y Detrend para TimeSeries y para Plot, ejemplo de uso

--- src/TimeSeries.py
+++ src/TimeSeries.py
@@ -1,5 +1,6 @@
-from typing import Tuple
+import warnings
 import numpy as np
+from statsmodels.tools.sm_exceptions import InterpolationWarning
 from statsmodels.tsa.stattools import adfuller, kpss
 
 class TimeSeries:
@@ -8,11 +9,56 @@ class TimeSeries:
         self.dates = dates
         self.time = np.arange(len(dates))
 
-    def detrend(self, grade: int = 1) -> Tuple["TimeSeries", np.ndarray]:
-        coef = np.polyfit(self.time, self.values, grade)
-        tendency = np.polyval(coef, self.time)
+    # --- --- --- Time Domain operations --- --- ---
+
+    def detrend_with_regression_fitting(self, grade: int = 1) -> "TimeSeries":
+        """
+        Eliminación de tendencia mediante modelo de regresión linael o polinomial.
+        """
+        tendency = self.tendency_with_regression_fitting(grade)
         detrended = self.values - tendency
-        return TimeSeries(self.time, detrended), tendency
+        return TimeSeries(self.dates, detrended)
+
+    def detrend_with_differencing(self, times: int = 1) -> "TimeSeries":
+        """
+        Eliminación de tendencia mediante diferenciación de `times` veces, con el objetivo de
+        hacer estacionaria una serie (por ej. para obtener el parámetro `d` del modelo ARIMA)
+        """
+        detrended = self.values.copy()
+        for _ in range(times):
+            detrended = np.diff(detrended)
+        new_date = self.dates[times:] # porque np.diff lo acorta
+        return TimeSeries(new_date, detrended)
+
+    def tendency_with_regression_fitting(self, grade: int = 1) -> np.array:
+        """
+        Línea de regresión lineal o polinomial.
+        """
+        coef = np.polyfit(self.time, self.values, grade)
+        return np.polyval(coef, self.time)
+
+    # --- --- --- Frequency Domain operations --- --- ---
+    def yearly_frequency_spectrum(self, freq_per_year=365):
+        """
+        Calcula el espectro de frecuencias (en ciclos por año) tomando `freq_per_year` como
+        el número de observaciones por año (365 si es diaria, 12 si es mensual, etc.)
+        """
+        n = len(self.values)
+        dt = 1 / freq_per_year  # intervalo en años entre observaciones
+
+        # FFT y frecuencias en ciclos por año
+        fft_result = np.fft.fft(self.values)
+        fft_freq = np.fft.fftfreq(n, d=dt)
+        magnitudes = np.abs(fft_result) / n
+
+        # Solo frecuencias positivas
+        mask = fft_freq > 0
+        frecuencias_pos = fft_freq[mask]
+        magnitudes_pos = magnitudes[mask]
+
+        return frecuencias_pos, magnitudes_pos
+
+    # --- --- --- Stationarity check --- --- ---
 
     def is_stationary(self) -> bool:
         # Test ADF
@@ -22,7 +68,9 @@ class TimeSeries:
         print('')
 
         # Test KPSS
-        kpss_result = kpss(self.values, regression='c', nlags='auto')
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=InterpolationWarning)
+            kpss_result = kpss(self.values, regression='ct', nlags='auto')
         print('KPSS Statistic:', kpss_result[0])
         print('p-value:', kpss_result[1])
         print('')
--- src/TimeSeriesPlot.py
+++ src/TimeSeriesPlot.py
@@ -1,4 +1,7 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.ticker import MaxNLocator, MultipleLocator
+
 from src import TimeSeries
 
 class TimeSeriesPlot:
@@ -6,36 +9,61 @@ class TimeSeriesPlot:
         self.ts = ts
         self.fig, self.ax = plt.subplots(figsize=(14, 6))
 
-    def addOriginal(self, color='orange'):
+    # --- --- Adición de plots en Dominio de Tiempo --- ---
+
+    def add_original(self, color='orange'):
         self.ax.plot(self.ts.dates, self.ts.values, label="Original", color=color)
-        self._setAxesForTimeDomain()
+        self._set_axes_for_time_domain()
 
-    def addTendency(self, grade=1, color='black', linestyle='--'):
-        _, tendency = self.ts.detrend(grade)
-        self.ax.plot(self.ts.dates, tendency, label=f'Tendencia (grado {grade})', color=color, linestyle=linestyle)
-        self._setAxesForTimeDomain()
-
-    def addDetrended(self, grade=1, color='gray'):
-        ts_detrended, _ = self.ts.detrend(grade)
+    def add_detrended(self, grade=1, color='gray'):
+        ts_detrended = self.ts.detrend_with_regression_fitting(grade)
         self.ax.plot(self.ts.dates, ts_detrended.values, label="Detrended", color=color)
-        self._setAxesForTimeDomain()
+        self._set_axes_for_time_domain()
 
-    def addFiltered(self, serie, corte, color='blue'):
+    def add_tendency(self, grade=1, color='black', linestyle='--'):
+        tendency = self.ts.tendency_with_regression_fitting(grade)
+        self.ax.plot(self.ts.dates, tendency, label=f'Tendencia (grado {grade})', color=color, linestyle=linestyle)
+        self._set_axes_for_time_domain()
+
+    def add_filtered(self, serie, corte, color='blue'):
         self.ax.plot(self.ts.dates, serie, label=f'Filtrada (corte={corte})', color=color, linewidth=2)
-        self._setAxesForTimeDomain()
+        self._set_axes_for_time_domain()
 
-    def addGrid(self):
+    def add_grid(self):
         self.ax.grid(True)
 
-    def setTitle(self, title):
+    def set_title(self, title):
         self.ax.set_title(title)
         
     def show(self):
         self.ax.legend()
         plt.show()
 
-    # --- --- --- --- --- --- --- --- ---
+    # --- --- Adición de plots en Dominio de Frecuencia --- ---
+    # Crear nuevo TimeSeriesPlot para esto, no usar el mismo que para dom de tiempo.
 
-    def _setAxesForTimeDomain(self):
+    def add_yearly_frequency_spectrum(self, low_freqs_limit=50, ticks_freq= 1, color='purple'):
+        frecuencias_pos, magnitudes_pos = self.ts.yearly_frequency_spectrum(365)
+
+        # Crear nueva figura para este plot
+        self.ax.plot(
+            frecuencias_pos[:low_freqs_limit],
+            magnitudes_pos[:low_freqs_limit],
+            color=color,
+            label=f'Frecuencia'
+        )
+        self.ax.set_title('Espectro de Frecuencias (ciclos/año)')
+        self.ax.xaxis.set_major_locator(MultipleLocator(ticks_freq))
+        self._set_axes_for_frequency_domain()
+        self.ax.grid(True)
+        plt.show()
+
+    # --- --- --- Privados --- --- ---
+
+    def _set_axes_for_frequency_domain(self):
+        self.ax.set_xlabel('Frecuencia (ciclos/año)')
+        self.ax.set_ylabel('Magnitud')
+
+    def _set_axes_for_time_domain(self):
         self.ax.set_xlabel("Fecha")
         self.ax.set_ylabel("Precio")
